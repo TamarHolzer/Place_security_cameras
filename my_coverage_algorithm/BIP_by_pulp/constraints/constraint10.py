3129e39a2abdb6dc11f2e341a8dac99b89d275e9
TamarHolzer <tamarsh7676@gmail.com>
2024-05-19 20:49:40 +0300

    pulp with out errors but maybe with mistakes

--- my_coverage_algorithm/BIP_by_pulp/constraints/constraint10.py
+++ my_coverage_algorithm/BIP_by_pulp/constraints/constraint10.py
@@ -2,7 +2,6 @@
 # Inequality constraint function
 import pulp
 def inequality_constraint3(prob, y, NA, NT, CVR):
-    # Add the constraint
-    prob += pulp.lpSum(y[k] for k in range(NA)) >= NT * CVR, "Minimum_coverage_rate_constraint"
-
+            prob += pulp.lpSum(y[k] for k in range(NT)) >= NT * CVR
+#        for k in range(NA):
 
--- my_coverage_algorithm/BIP_by_pulp/constraints/constraint8.py
+++ my_coverage_algorithm/BIP_by_pulp/constraints/constraint8.py
@@ -4,36 +4,21 @@ import pulp
 
 def inequality_constraint1(prob, x, y, v, NC, NhD, NvD, NE, NA, NT):
     for k in range(NT):
-        prob += pulp.lpSum(v[i, j, d, e, t, k] * x[(i, j, d, e, t)]
-                           for i in range(NC)
-                           for j in range(NhD)
-                           for d in range(NvD)
-                           for e in range(NE)
-                           for t in range(NA)) >= y[k], f"Coverage_constraint_for_target_{k}"
+        prob += pulp.lpSum(v[i][j][d][e][t][k] * x[i, j, d, e, t] for i in range(NC) for j in range(NhD)
+                           for d in range(NvD) for e in range(NE) for t in range(NA)) >= y[k]
 
-    # # Iterate over all targets k
-    # # x_reshaped = numpy.reshape(x, (NC, NhD, NvD, NE, NA))
-    #
-    # # for k in range(NT):
-    # #     prob += (pulp.lpSum(v[i][j][d][e][t][k] * x[(i, j, d, e, t)]
-    # #                         for i in range(NC)
-    # #                         for j in range(NhD)
-    # #                         for d in range(NvD)
-    # #                         for e in range(NE)
-    # #                         for t in range(NA)) >= y[k], f"Coverage constraint for target {k}")
     # for k in range(NT):
-    #     prob += (pulp.lpSum(v[i, j, d, e, t, k] * x[(i, j, d, e, t)]
-    #                         for i in range(NC)
-    #                         for j in range(NhD)
-    #                         for d in range(NvD)
-    #                         for e in range(NE)
-    #                         for t in range(NA)) <= NC * y[k])
-    #
-    # # # Iterate over all combinations of indices i, j, d, e, and t
-    # # for i in range(NC):
-    # #     for j in range(NhD):
-    # #         for d in range(NvD):
-    # #             for e in range(NE):
-    # #                 for t in range(NA):
-    # #                     # Compute the product of decision variable x and other variable V
-    # #                      prob += pulp.lpSum(v[i][j][d][e][t][k] * x[i][j][d][e][t]) >= y[k], f"Coverage constraint for target {k}"
+    #     for i in range(NC):
+    #         for j in range(NhD):
+    #             for d in range(NvD):
+    #                 for e in range(NE):
+    #                     for t in range(NA):
+    #                          prob += (v[i][j][d][e][t][k] * x[(i, j, d, e, t)]) >= y[k], f"Coverage_constraint_for_target_{k}"
+    # for k in range(NT):
+    #     prob += pulp.lpSum(v[i, j, d, e, t, k] * x[(i, j, d, e, t)]
+    #                        for i in range(NC)
+    #                        for j in range(NhD)
+    #                        for d in range(NvD)
+    #                        for e in range(NE)
+    #                        for t in range(NA)) >= y[k], f"Coverage_constraint_for_target_{k}"
+
