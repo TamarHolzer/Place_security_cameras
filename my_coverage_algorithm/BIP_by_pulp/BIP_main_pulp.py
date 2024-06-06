2b7ef1e52a117b294b1398c0ef5eaa46a8fe6776
TamarHolzer <tamarsh7676@gmail.com>
2024-06-06 19:22:24 +0300

    Placing the data in V may not be correct

--- my_coverage_algorithm/BIP_by_pulp/BIP_main_pulp.py
+++ my_coverage_algorithm/BIP_by_pulp/BIP_main_pulp.py
@@ -1,50 +1,50 @@
-#import solve_with_pulp
+import solve_with_pulp
 import cameras.retrieving_the_cameras_data as dataFuncs
 from my_coverage_algorithm.BIP_by_pulp import find_room2
-from cameras import Camera
+from my_coverage_algorithm import Initialize_the_quantity_variable as init
 
+def BIP_main(list_of_tuples_with_the_xy_cordinates = [(0, 0), (9, 0), (9, 9), (0, 9)], heightOfRoomChosenByUser = 2.50):#x,v,y,nc, nhd, nvd, ne, na,nt
 
-# import numpy
-def BIP_main(list_of_tuples_with_the_xy_cordinates = [(0, 0), (9, 0), (9, 9), (0, 9)]):#x,v,y,nc, nhd, nvd, ne, na,nt
     #שליפת נתוני המצלמות
     dataCameras, numOfCameras = dataFuncs.get_all_cameras()
+    pointInWalls = find_room2.find_room_frame(list_of_tuples_with_the_xy_cordinates)  # מיקומים אפשריים למצלמות
+
+    # number of target positions- מספר עמדות יעד
+    NT = init.sum_the_target_position(pointInWalls)
 
     # number of camera positions- מספר עמדות המצלמה
-    pointInWalls = find_room2.find_room_frame(list_of_tuples_with_the_xy_cordinates)#מיקומים אפשריים למצלמות
-    children_len = [len(child) for child in pointInWalls] #סכימת הנקודות
-    NC = sum(children_len)
-    # number of horizontal orientations- מספר כיוונים אופקיים.
-    NhD = 2
+    NC = init.sum_the_cameras_positions(pointInWalls)
+
+
     # number of vertical orientations- מספר כיוונים אנכיים0
     numOfVerticalOriens = []
     if dataCameras != None:
         for i in range(len(dataCameras)):
             theVer = dataCameras[i]
             numOfVerticalOriens.append(theVer.vertical_angle)
-    else:
-        print("Make sure that PYCHARM is open as an administrator")
-    NvD = max(numOfVerticalOriens)
-    #print(NvD)
+    if (numOfVerticalOriens != []):
+        numOfVerticalOriens.sort(reverse=True)
+        NvD = numOfVerticalOriens.pop()
+    # print(NvD)
+
     # number of heights- מספר גבהים
-    NE = 1
+    heightOfRoomChosenByUser *= 100
+    if (heightOfRoomChosenByUser > 280):
+        NE = 1
+    else:
+        NE = 280 - heightOfRoomChosenByUser
+
     # number of camera types- מספר סוגי מצלמות
     NA = numOfCameras
-    # number of target positions- מספר עמדות יעד
-    dirOfTheTargetPoints = find_room2.find_room_targets(pointInWalls)
-    numOfTheTargetPoints = 0
-    listOfTargetPositions = []
-    #המרת הנקודות לרשימה
-    for key, tuple_list in dirOfTheTargetPoints.items():
-        #print(f"Key: {key}")
-        for tuple_item in tuple_list:
-            numOfTheTargetPoints += 1
-            listOfTargetPositions.append(tuple_item)
-            #print(f"  Tuple: {tuple_item}")
-    #print(listOfTargetPositions)
-    NT = numOfTheTargetPoints
+
+    # number of horizontal orientations- מספר כיוונים אופקיים.
+
+    NhD = int(sum_of_horizontal_orientations(list_of_tuples_with_the_xy_cordinates, את הזווית האופקית המינימלית))
+
 
     # given minimal coverage rate- בהינתן שיעור כיסוי מינימלי
     CVR = 0.9
+
 
     sol = solve_with_pulp.solve_with_pulp(NC=NC, NhD=NhD, NvD=NvD, NE=NE, NA=NA, NT=NT, CVR=CVR)
 
--- my_coverage_algorithm/main.py
+++
@@ -1,9 +0,0 @@
-#def main():
-    #constraint8(x, v, y, NT, NC, NhD, NvD, NE, NA):
-   # x, v, y = []
-    #options = find_room.find_room((0, 0), (8, 8), (0, 16))
-
-####לסדר את באילוצים עם ההפחתה של צד ימין
-
-
-#main()
